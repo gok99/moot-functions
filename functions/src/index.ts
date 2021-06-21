8153d9bc8d9058160aa18643ce81acbd141ee3d0
Gokul Rajiv <gokul.rajiv99@gmail.com>
2021-06-21 23:11:01 +0800

    setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable require-jsdoc */
 import * as functions from "firebase-functions";
+import {diff} from "./utils";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +10,14 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+export const onUserChanged = functions.database
+    .ref("/users/{uid}/queue")
+    .onUpdate((snapshot, context) => {
+      const uid = context.params.uid;
+      console.log(`${uid} data changed`);
+      const beforeUserData = snapshot.before.val();
+      const afterUserData = snapshot.after.val();
+      console.log(diff(beforeUserData, afterUserData));
+    });
+
---
+++ functions/src/utils.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable require-jsdoc */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export function diff(
+    obj1: any,
+    obj2: any
+): { [key: string]: any } | undefined {
+  const result: { [key: string]: any } = {};
+  if (Object.is(obj1, obj2)) {
+    return undefined;
+  }
+  if (!obj2 || typeof obj2 !== "object") {
+    return obj2;
+  }
+  Object.keys(obj1 || {})
+      .concat(Object.keys(obj2 || {}))
+      .forEach((key) => {
+        if (obj2[key] !== obj1[key] && !Object.is(obj1[key], obj2[key])) {
+          result[key] = obj2[key];
+        }
+        if (typeof obj2[key] === "object" && typeof obj1[key] === "object") {
+          const value = diff(obj1[key], obj2[key]);
+          if (value !== undefined) {
+            result[key] = value;
+          }
+        }
+      });
+  return result;
+}
